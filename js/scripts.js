fc82a88aabc5817a64a8878c04940b48ca73efbd
Eric <eric.melo72@protonmail.com>
2026-03-13 17:57:58 -0300

    Changend animations | Image color switch added | Project finished

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,29 @@ elements.forEach((element) => {
 btn.forEach((element) => {
   myObserver.observe(element);
 });
+
+// Color switch
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.querySelector("#product-image");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log(e);
+
+    buttons.forEach((btn) => {
+      btn.querySelector(".color").classList.remove("selected");
+    });
+
+    const button = e.target;
+    const id = button.getAttribute("id");
+
+    button.querySelector(".color").classList.add("selected");
+
+    image.classList.add("changing");
+    image.setAttribute("src", `img/iphone_${id}.webp`);
+
+    setTimeout(() => {
+      image.classList.toggle("changing");
+    }, 100);
+  });
+});
